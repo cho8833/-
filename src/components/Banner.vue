8743d005020ad67339f458a558491b82a76e9ae4
cho8833 <chowsp1009@gmail.com>
2024-10-09 19:41:24 +0900

    banner

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -1,6 +1,20 @@
 <template>
-    <div :style="{backgroundImage: 'url(@/assets/banner.png)'}">
-
+    <div class="banner">
+        <img :src="banner">
+        <div class="banner-title-box">
+            <div style="height: 82px"/>
+            <div class="banner-title-1">
+                MMCA Research Lab
+            </div>
+            <div style="height: 18px"/>
+            <div class="banner-title-2">
+                한국미술 연구 온라인 플랫폼
+            </div>
+            <div style="height: 22px"/>
+            <div class="banner-title-3">
+                Online Platform for Korean Art Research
+            </div>
+        </div>
     </div>
 </template>
 
@@ -9,4 +23,28 @@
 </script>
 
 <style>
+    .banner {
+        position: relative;
+    }
+    .banner-title-box {
+        position: absolute;
+        padding-left: 151px;
+        height: 100%;
+        width: 100%;
+        top: 0;
+        color: #fcf6e4;
+        line-height: 1;
+    }
+    .banner-title-1 {
+        font-size: 30px;
+        font-weight: 600;
+    }
+    .banner-title-2 {
+        font-size: 40px;
+        font-weight: 600;
+    }
+    .banner-title-3 {
+        font-size: 18px;
+        font-weight: 300;
+    }
 </style>
